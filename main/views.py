913464f9f3a30577c3fe559d09a8711838408252
Michał <michal.kalicki@outlook.com>
2024-07-21 17:53:32 +0200

    Menu page created

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, response
+from django.views import View
 
-# Create your views here.
+
+
+class HomePageView(View):
+    def get(self, request):
+        return render(request, 'home.html')
+
