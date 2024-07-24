e33c70370a100a5867fd999cff0c1806ed46dc03
Michał <michal.kalicki@outlook.com>
2024-07-24 19:23:48 +0200

    'Add new room' View

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from main.views import HomePageView
+from main.views import HomePageView, NewRoomView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view()),
+    path('room/new/', NewRoomView.as_view()),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, response
+from django.http import HttpResponse
 from django.views import View
 
-
+from main.models import Room
 
 class HomePageView(View):
     def get(self, request):
         return render(request, 'home.html')
+
+class NewRoomView(View):
+    def get(self, request):
+        return render(request, 'new_room.html')
+
+    def post(self, request):
+        name = request.POST.get('name')
+        capacity = int(request.POST.get('capacity'))
+        projector = int(request.POST.get('projector'))
+        if name and capacity > 0:
+            if not Room.objects.filter(name=name).exists():
+                Room.objects.create(name=name, capacity=capacity, have_projector=projector)
+                return redirect('http://127.0.0.1:8000/')
+            else:
+                message = 'Error: Room already exists'
+                return render(request, 'new_room.html', {'message': message})
+        else:
+            message = 'Error: Empty field or negative room capacity'
+            return render(request, 'new_room.html', {'message': message})
+
+
 
