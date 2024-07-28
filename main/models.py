4df9023a677557f5b5199536015840e2361bcbb4
Michał <michal.kalicki@outlook.com>
2024-07-29 01:20:34 +0200

    Booking model

--- main/models.py
+++ main/models.py
@@ -5,3 +5,12 @@ class Room(models.Model):
     name = models.CharField(max_length=255, unique=True)
     capacity = models.IntegerField()
     have_projector = models.BooleanField()
+
+
+class Booking(models.Model):
+    date = models.DateField()
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+    class Meta:
+        unique_together = (('room_id', 'date'),)
